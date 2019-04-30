c6c5c920bd60dad0cde9da7fe0b00a4be386a27b
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-30 21:12:10 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_04_30_102431) do
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.boolean "published"
