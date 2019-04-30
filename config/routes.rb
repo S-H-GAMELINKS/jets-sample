b67dd226d09970980d92f7bb9d65bb8ed9b76b7a
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-30 22:48:20 +0900

    fix root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Jets.application.routes.draw do
+  root 'posts#index'
   resources :posts
-  root "jets/public#show"
 
   # The jets/public#show controller can serve static utf8 content out of the public folder.
   # Note, as part of the deploy process Jets uploads files in the public folder to s3
